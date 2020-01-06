b99754d587bd75c47c53a31e1db2060356a28c51
tongpinmo <1529901761@qq.com>
2020-01-06 09:07:45 +0800

    add

--- tools/inference.sh
+++ tools/inference.sh
@@ -1,1 +1,1 @@
-python inference.py --data_dir='/mnt/a409/SIBITU/SIBITU_Dataset/SensynRobotics/ss xt2 1012/5/101MEDIA/ir/'
+python inference.py --data_dir='/mnt/a409/SIBITU/SIBITU_Dataset/SIBITU_AI/train-solarpannel/images'
